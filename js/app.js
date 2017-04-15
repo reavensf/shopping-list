2d9b28e38c0c296c7e6084c0a203c01b8e39acc0
reavensf <rhfenelon@gmail.com>
2017-04-15 01:39:37 -0400

    Commiting the final files

--- js/app.js
+++ js/app.js
@@ -17,7 +17,8 @@ $(function(){
     // Render functions
     var renderList = function(state, element){
         var itemsHTML = state.items.map(function(item){
-            var controls = '<div class="shopping-item-controls"> <button class="shopping-item-toggle"> <span class="button-label">check</span> </button> <button class="shopping-item-delete"> <span class="button-label">delete</span> </button> </div>'
+            var controls = '<div class="shopping-item-controls"> <button class="shopping-item-toggle"> <span class="button-label">check</span> </button> <button class="shopping-item-delete"> <span class="button-label">delete</span> </button> </div>';
+
             return '<li>' + '<span class="shopping-item">' + item + '</span>' + controls + '</li>';
         });
         element.html(itemsHTML);
@@ -27,19 +28,18 @@ $(function(){
     $('#js-shopping-list-form').submit(function(event){
         event.preventDefault();
         addItem(state, $('#shopping-list-entry').val());
-        renderList(state, $('.shopping-list'))
+        renderList(state, $('.shopping-list'));
+        $('#shopping-list-entry').val('');
     });
 
     $(document).on('click', '.shopping-item-toggle', function(event){
-        // event.preventDefault();
-        console.log('checked clicked');
         $(this).parent('.shopping-item-controls').parent('li').find('.shopping-item').toggleClass('shopping-item__checked');
     });
 
     $(document).on('click', '.shopping-item-delete', function(event){
-        console.log('delete clicked');
         removeItem(state, $(this).parent('.shopping-item-controls').parent('li').find('.shopping-item').text());
-        $(this).parent('.shopping-item-controls').parent('li').remove();
+        renderList(state, $('.shopping-list'));
+
         console.log(state);
     });
 
